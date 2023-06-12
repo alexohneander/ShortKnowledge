0b2183804675eb5ee5187ce46126ee53c6660c94
Alex Wellnitz <alex.wellnitz@materna.group>
2023-06-12 14:03:53 +0200

    Update SummarizeService, add GetChatCompletionsAsync

--- Services/SummarizeService.cs
+++ Services/SummarizeService.cs
@@ -11,14 +11,22 @@ public static class SummarizeService
 
     public static async Task<string> Summarize(ShortRequestViewModel model)
     {
-        string query = "Fasse mir folgenden Text auf maximal 100 Wörter und auf deutsch zusammen: " + model.Text;
-
         OpenAIClient client = new OpenAIClient(ApiKey);
 
-        string deploymentName = "text-davinci-003";
-        string prompt = query;
-        Response<Completions> completionsResponse = await client.GetCompletionsAsync(deploymentName, prompt);
-        string completion = completionsResponse.Value.Choices[0].Text;
+        string deploymentName = "gpt-3.5-turbo";
+        string query = "Fasse mir folgenden Text auf maximal 100 Wörter und auf deutsch zusammen: " + model.Text;
+        
+        var message = new ChatMessage( ChatRole.User, query);
+        List<ChatMessage> messages = new List<ChatMessage> { message };
+
+        ChatCompletionsOptions options = new ChatCompletionsOptions
+        {
+            MaxTokens = 100,
+        };
+        options.Messages.Add(message);
+
+        Response<ChatCompletions> completionsResponse = await client.GetChatCompletionsAsync(deploymentName, options);
+        string completion = completionsResponse.Value.Choices[0].Message.Content;
 
         Console.WriteLine($"Chatbot: {completion}");
 
